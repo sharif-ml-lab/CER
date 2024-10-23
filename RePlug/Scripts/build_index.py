f16f224028d62ac4e26760de69b630287b18a9a8
a.razghandi <a.razghandi11@gmail.com>
2024-10-23 14:38:29 +0330

    Add deepspeed

--- RePlug/Scripts/build_index.py
+++ RePlug/Scripts/build_index.py
@@ -3,6 +3,8 @@ import faiss
 from datasets import load_from_disk
 from transformers import AutoTokenizer, AutoModel
 from tqdm import tqdm
+import deepspeed
+
 
 def embed_text_batch(chunks, tokenizer, model, device='cuda'):
     # Tokenize chunks as a batch
@@ -11,6 +13,7 @@ def embed_text_batch(chunks, tokenizer, model, device='cuda'):
         # Get embeddings for the batch
         embeddings = model(**inputs).last_hidden_state.mean(dim=1)  # mean pooling of the last hidden of the tokens
     return embeddings.detach().cpu().numpy()
+
 
 if __name__ == '__main__':
     # Load the wiki dataset from HuggingFace
@@ -23,13 +26,16 @@ if __name__ == '__main__':
     model = AutoModel.from_pretrained(model_name).to('cuda')
     model.eval()
 
+    # Initialize DeepSpeed-Inference
+    ds_model = deepspeed.init_inference(model, mp_size=1)
+
     # Prepare FAISS index
     dimension = model.config.hidden_size
     index = faiss.IndexFlatL2(dimension)
 
     # Define batch size
-    batch_size = 16
-    text_batch_size = 64
+    batch_size = 1024
+    text_batch_size = 1024
     all_texts = []
 
     # Collect all texts from the dataset for batch tokenization
@@ -40,7 +46,8 @@ if __name__ == '__main__':
     all_chunks = []
     for i in tqdm(range(0, len(all_texts), text_batch_size), desc="Tokenizing texts in batches"):
         batch_texts = all_texts[i:i + text_batch_size]
-        batch_tokens = tokenizer(batch_texts, return_tensors="pt", truncation=True, padding=True, max_length=128, add_special_tokens=False)
+        batch_tokens = tokenizer(batch_texts, return_tensors="pt", truncation=True, padding=True, max_length=128,
+                                 add_special_tokens=False)
         for j in range(len(batch_texts)):
             tokens = batch_tokens.input_ids[j]
             chunks = [tokens[k:k + 128] for k in range(0, len(tokens), 128)]
@@ -50,7 +57,7 @@ if __name__ == '__main__':
     # Process chunks in batches to create embeddings and add to FAISS index
     for i in tqdm(range(0, len(all_chunks), batch_size), desc="Embedding and indexing batches"):
         batch_chunks = all_chunks[i:i + batch_size]
-        embeddings = embed_text_batch(batch_chunks, tokenizer, model)
+        embeddings = embed_text_batch(batch_chunks, tokenizer, ds_model)
         index.add(embeddings)
 
     # Save the FAISS index for future retrieval
