816994194c1fc2993157679522831cfac0965072
Aquasar11 <a.razghandi11@gmail.com>
2024-11-11 07:44:38 +0000

    Final  version befor refactoring

--- CoT_Decoding/main.py
+++ CoT_Decoding/main.py
@@ -5,11 +5,42 @@ from datasets import load_dataset
 from tqdm import tqdm
 
 # Load the model and tokenizer
-model_name = "/data/TensorRT-LLM/Meta-Llama-3.1-70B-Instruct"
+# model_name = "/data/TensorRT-LLM/Meta-Llama-3.1-70B-Instruct"
+model_name = "/data/models/Meta-Llama-3.1-8B-Instruct"
+
 model = AutoModelForCausalLM.from_pretrained(model_name, local_files_only=True, device_map='auto', torch_dtype=torch.bfloat16)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-def multiarith():
+def construct_prompt(question):
+    base = f"""Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
+A: There are 15 trees originally. Then there were 21 trees after some more were planted. So there must have been 21 - 15 = 6. The answer is 6.
+
+Q: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
+A: There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5. The answer is 5.
+
+Q: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
+A: Originally, Leah had 32 chocolates. Her sister had 42. So in total they had 32 + 42 = 74. After eating 35, they had 74 - 35 = 39. The answer is 39.
+
+Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
+A: Jason started with 20 lollipops. Then he had 12 after giving some to Denny. So he gave Denny 20 - 12 = 8. The answer is 8.
+
+Q: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
+A: Shawn started with 5 toys. If he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9. The answer is 9.
+
+Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
+A: There were originally 9 computers. For each of 4 days, 5 more computers were added. So 5 * 4 = 20 computers were added. 9 + 20 is 29. The answer is 29.
+
+Q: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
+A: Michael started with 58 golf balls. After losing 23 on tuesday, he had 58 - 23 = 35. After losing 2 more, he had 35 - 2 = 33 golf balls. The answer is 33.
+
+Q: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
+A: Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The answer is 8.
+
+Q: {question}
+A: """
+    return base
+
+def multiarith(k, aggregate, decoding_mode):
     # Load the MultiArith dataset
     dataset = load_dataset("ChilleD/MultiArith")
 
@@ -27,12 +58,12 @@ def multiarith():
             
             # Prepare the message for the model
             messages = [
-                {"role": "user", "content": question}
+                {"role": "user", "content": construct_prompt(question)}
             ]
             
             # Generate the response using CoT decoding
-            result, confidence, final_ans = cot_decode(model, tokenizer, messages, aggregate_paths=False, max_new_tokens=512, k=10,
-                                                    decoding_mode='baseline')
+            result, confidence, final_ans = cot_decode(model, tokenizer, messages, aggregate_paths=aggregate, max_new_tokens=512, k=k,
+                                                    decoding_mode=decoding_mode)
             
             # Compare the model's answer with the correct answer
             try:
@@ -54,7 +85,7 @@ def multiarith():
     print(f"Final Accuracy: {accuracy:.2f}%")
     return accuracy
 
-def gsm8k():
+def gsm8k(k, aggregate, decoding_mode):
     # Load the GSM8K dataset
     dataset = load_dataset("openai/gsm8k", "main")
 
@@ -83,12 +114,12 @@ def gsm8k():
 
             # Prepare the message for the model
             messages = [
-                {"role": "user", "content": question}
+                {"role": "user", "content": construct_prompt(question)}
             ]
             
             # Generate the response using CoT decoding
-            result, confidence, final_ans = cot_decode(model, tokenizer, messages, aggregate_paths=False, max_new_tokens=512, k=10,
-                                                    decoding_mode='baseline')
+            result, confidence, final_ans = cot_decode(model, tokenizer, messages, aggregate_paths=aggregate, max_new_tokens=512, k=k,
+                                                    decoding_mode=decoding_mode)
             
             # Compare the model's answer with the correct answer
             try:
@@ -113,5 +144,14 @@ def gsm8k():
 
 if __name__ == '__main__':
 
-    multiarith()
-    gsm8k()
+    K = 10
+    AGGREGATE = False
+    DECODING_MODE = 'baseline'
+
+    print(model_name)
+    print(f'Mode: CoT + {DECODING_MODE}')
+    print(f'Config: k = {K}, Aggregate = {AGGREGATE}')
+
+
+    multiarith(k=K, aggregate=AGGREGATE, decoding_mode=DECODING_MODE)
+    gsm8k(k=K, aggregate=AGGREGATE, decoding_mode=DECODING_MODE)
